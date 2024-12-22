8ef37d2539cc95ab115b47c27ad45f0732bc0f91
ivanjermakov <ivanjermakov1@gmail.com>
2024-12-22 14:02:40 +0100

    Match abs setup of source device

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
-use std::{any::Any, fs::canonicalize, path::PathBuf};
+use std::{collections::HashMap, fs::canonicalize, path::PathBuf};
 
 use anyhow::{Context, Result};
 use clap::{value_parser, Parser};
 use evdev::{
     uinput::{VirtualDevice, VirtualDeviceBuilder},
     AbsInfo, AbsoluteAxisCode, AbsoluteAxisEvent, AttributeSet, BusType, Device,
-    EventSummary::{self, *},
+    EventSummary::*,
     InputId, KeyCode, UinputAbsSetup,
 };
 
@@ -25,7 +25,7 @@ fn main() -> Result<()> {
     let mut device = Device::open(path)?;
     println!("{:?}", device.input_id());
 
-    let mut virt_device = make_virt_device()?;
+    let mut virt_device = make_virt_device(&device)?;
     for path in virt_device.enumerate_dev_nodes_blocking()? {
         println!("virt device available as {}", path?.display());
     }
@@ -34,19 +34,23 @@ fn main() -> Result<()> {
         for ev in device.fetch_events()? {
             match ev.destructure() {
                 Synchronization(..) => continue,
-                Key(key_event, key_code, _) => println!("{:?} {:?}", key_event, key_code),
+                Key(key_event, key_code, _) => {
+                    println!("{:?} {:?}", key_event, key_code)
+                }
                 AbsoluteAxis(event, code, value) => {
-                    println!("{:?} {:?} {}", event.event_type(), code, value);
+                    // println!("{:?} {:?} {}", event.event_type(), code, value);
                     let virt_ev = *AbsoluteAxisEvent::new(code, value);
                     virt_device.emit(&[virt_ev])?;
                 }
-                evs => println!("other {:?}", EventSummary::type_id(&evs)),
+                _ => {
+                    // println!("other {:?}", ev)
+                }
             }
         }
     }
 }
 
-fn make_virt_device() -> Result<VirtualDevice> {
+fn make_virt_device(device: &Device) -> Result<VirtualDevice> {
     let xbox_id = InputId::new(BusType::BUS_USB, 0x45e, 0x28e, 0x101);
     let xbox_name = "Microsoft X-Box 360 pad";
     let mut keys = AttributeSet::<KeyCode>::new();
@@ -65,20 +69,31 @@ fn make_virt_device() -> Result<VirtualDevice> {
     ] {
         keys.insert(key);
     }
-    let axis_max = 256;
-    let abs_setup = AbsInfo::new(axis_max / 2, 0, axis_max, 0, 0, 1);
+    let abs_infos = device
+        .get_absinfo()?
+        .collect::<HashMap<AbsoluteAxisCode, AbsInfo>>();
     let virt_device = VirtualDeviceBuilder::new()?
         .name(xbox_name)
         .input_id(xbox_id)
         .with_keys(&keys)?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_X, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_Y, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_Z, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_RX, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_RY, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_RZ, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_HAT0X, abs_setup))?
-        .with_absolute_axis(&UinputAbsSetup::new(AbsoluteAxisCode::ABS_HAT0Y, abs_setup))?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_X, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_Y, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_Z, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_RX, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_RY, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_RZ, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_HAT0X, &abs_infos)?)?
+        .with_absolute_axis(&abs_setup(AbsoluteAxisCode::ABS_HAT0Y, &abs_infos)?)?
         .build()?;
     Ok(virt_device)
+}
+
+fn abs_setup(
+    code: AbsoluteAxisCode,
+    abs_infos: &HashMap<AbsoluteAxisCode, AbsInfo>,
+) -> Result<UinputAbsSetup> {
+    let axis_max = 256;
+    let default_info = AbsInfo::new(axis_max / 2, 0, axis_max, 0, 0, 1);
+    let info = abs_infos.get(&code).cloned().unwrap_or(default_info);
+    Ok(UinputAbsSetup::new(code, info))
 }
