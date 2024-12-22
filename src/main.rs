911edaac3da5dd8d064bfb01abc3142f3bb9df6b
ivanjermakov <ivanjermakov1@gmail.com>
2024-12-22 01:38:19 +0100

    Nicer hex output

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,17 @@
+use core::fmt;
 use std::{
     fs::{canonicalize, read_to_string},
     path::{Path, PathBuf},
-    thread::sleep,
-    time::Duration,
 };
 
 use anyhow::{Context, Result};
 use clap::{value_parser, Parser};
 
+use crate::hex::{hex, hex_padded};
+
 extern crate hidapi;
+
+mod hex;
 
 #[derive(Parser)]
 #[command(name = "path-parser")]
@@ -24,6 +27,17 @@ struct HidId {
     product: u16,
 }
 
+impl fmt::Display for HidId {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "{}:{}",
+            hex(&self.vendor.to_be_bytes()),
+            hex(&self.product.to_be_bytes())
+        )
+    }
+}
+
 fn main() -> Result<()> {
     let Args { path } = Args::try_parse()?;
 
@@ -31,28 +45,27 @@ fn main() -> Result<()> {
     println!("path {path:?}");
 
     let id = find_id(&path).context(format!("unable to extract id {path:?}"))?;
-    println!("id {id:?}");
+    println!("id {id}");
 
     let hid_api = hidapi::HidApi::new()?;
     let device = hid_api
         .open(id.vendor, id.product)
         .context("cannot open device")?;
 
-    let mut rd_buffer = [0u8; 1 << 12];
+    let mut rd_buffer = [0u8; 1 << 10];
     let rd_size = device.get_report_descriptor(&mut rd_buffer)?;
     let rd_buffer = rd_buffer[..rd_size].to_vec();
-    print_hex(&rd_buffer);
+    println!("{}", hex_padded(&rd_buffer));
 
-    let mut buffer = [0u8; 12];
+    let mut buffer = [0u8; 1 << 8];
     loop {
         match device.read(&mut buffer) {
             Ok(0) => break,
             Ok(bytes_read) => {
-                print_hex(&buffer[..bytes_read]);
+                println!("{}", hex_padded(&buffer[..bytes_read]));
             }
             Err(e) => eprintln!("Error reading from device: {}", e),
         }
-        sleep(Duration::from_millis(10));
     }
 
     Ok(())
@@ -83,7 +96,7 @@ fn find_id(dev_path: &Path) -> Result<HidId> {
     })
 }
 
-fn print_hex(data: &[u8]) {
+fn to_hex(data: &[u8]) {
     for byte in data {
         print!("{:02X} ", byte);
     }
